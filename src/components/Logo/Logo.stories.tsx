af6a92f55ae680b39b220fd743ee5ca3f2ad1220
ruanvsrateira <vruan85@gmail.com>
2023-10-23 23:45:15 -0300

    feat: create component Logo

---
+++ src/components/Logo/Logo.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Logo } from "./Logo";
+
+const meta = {
+  title: "Components/Logo",
+  component: Logo,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Logo>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const Main: Story = {};
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,5 @@
+import LogoSource from "../../assets/logo.png";
+
+export const Logo = () => {
+  return <img src={LogoSource} alt="logo" />;
+};
