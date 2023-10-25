4efc5fcf6366da9ed12c594adebbe06c4cbd2f86
ruanvsrateira <vruan85@gmail.com>
2023-10-24 23:48:16 -0300

    feat: configuring routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
 import { CreatePost } from "../pages/CreatePost/CreatePost";
+import { DeletePost } from "../pages/DeletePost/DeletePost";
+import { UpdatePost } from "../pages/UpdatePost/UpdatePost";
+import { PostDetails } from "../pages/PostDetails/PostDetails";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   {
     path: "/create-post",
     element: <CreatePost />,
+  },
+  {
+    path: "/delete-post",
+    element: <DeletePost />,
+  },
+  {
+    path: "/update-post",
+    element: <UpdatePost />,
+  },
+  {
+    path: "/post-details",
+    element: <PostDetails />,
   },
 ]);
