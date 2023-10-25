583503a55b7b5f9a59a1008630e9f3378352d93e
ruanvsrateira <vruan85@gmail.com>
2023-10-24 21:43:55 -0300

    feat: create component form

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,239 @@
+import { Box, Button, useMediaQuery } from "@mui/material";
+import { ArrowBack, Check } from "@mui/icons-material";
+import theme from "../../theme";
+import { Input } from "../Input/Input";
+import { FormHTMLAttributes } from "react";
+import { FormikProps } from "formik";
+import { Link } from "react-router-dom";
+import { IPostSchema } from "../../schemas/post.schema";
+
+interface FormProps extends FormHTMLAttributes<HTMLFormElement> {
+  formik: FormikProps<IPostSchema>;
+}
+
+export const Form = ({ formik, ...props }: FormProps) => {
+  const breaker800px = useMediaQuery("(max-width: 800px)");
+
+  return (
+    <form {...props}>
+      <Box
+        display="flex"
+        alignItems="center"
+        gap={2}
+        style={
+          breaker800px
+            ? {
+                flexDirection: "column",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }
+            : {}
+        }
+      >
+        <Input
+          label="Título da Postagem"
+          fullWidth
+          name="title"
+          error={!!formik.errors.title}
+          helperText={formik.errors.title}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.title}
+        />
+        <Input
+          label="Título SEO"
+          fullWidth
+          name="seo_title"
+          value={formik.values.seo_title}
+          error={!!formik.errors.seo_title}
+          helperText={formik.errors.seo_title}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        <Input
+          label="Tags(usar separador como a ,)"
+          fullWidth
+          name="tags"
+          value={formik.values.tags}
+          error={!!formik.errors.tags}
+          helperText={formik.errors.tags}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+      </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        gap={2}
+        mt={2}
+        style={
+          breaker800px
+            ? {
+                flexDirection: "column",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }
+            : {}
+        }
+      >
+        <Input
+          label="Link do Youtube"
+          fullWidth
+          name="youtube_link"
+          value={formik.values.youtube_link}
+          error={!!formik.errors.youtube_link}
+          helperText={formik.errors.youtube_link}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        <Input
+          label="Tags SEO"
+          fullWidth
+          name="seo_tags"
+          value={formik.values.seo_tags}
+          error={!!formik.errors.seo_tags}
+          helperText={formik.errors.seo_tags}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+      </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        gap={2}
+        mt={2}
+        style={
+          breaker800px
+            ? {
+                flexDirection: "column",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }
+            : {}
+        }
+      >
+        <Input
+          label="Texto Primário"
+          fullWidth
+          name="text_primary"
+          value={formik.values.text_primary}
+          error={!!formik.errors.text_primary}
+          helperText={formik.errors.text_primary}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        <Input
+          label="Text Secundário"
+          fullWidth
+          name="text_secondary"
+          value={formik.values.text_secondary}
+          error={!!formik.errors.text_secondary}
+          helperText={formik.errors.text_secondary}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+      </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        gap={2}
+        mt={2}
+        style={
+          breaker800px
+            ? {
+                flexDirection: "column",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }
+            : {}
+        }
+      >
+        <Input
+          title="Plfeafaef"
+          label="Publicado em"
+          placeholder="Publicado em"
+          fullWidth
+          type="datetime-local"
+          name="published_at"
+          value={formik.values.published_at}
+          error={!!formik.errors.published_at}
+          helperText={formik.errors.published_at}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        <Input
+          fullWidth
+          label="destaque até"
+          type="datetime-local"
+          name="featured_until"
+          value={formik.values.featured_until}
+          error={!!formik.errors.featured_until}
+          helperText={formik.errors.featured_until}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+      </Box>
+
+      <Box
+        display="flex"
+        gap={2}
+        mt={4}
+        style={
+          breaker800px
+            ? {
+                flexDirection: "column",
+                justifyContent: "center",
+                width: "100%",
+              }
+            : {}
+        }
+      >
+        <Link to="/">
+          <Button
+            variant="contained"
+            style={
+              breaker800px
+                ? {
+                    justifyContent: "center",
+                    width: "100%",
+                    height: 50,
+                    backgroundColor: theme.colors["main-red"],
+                  }
+                : {
+                    backgroundColor: theme.colors["main-red"],
+                  }
+            }
+            startIcon={<ArrowBack />}
+            size="large"
+          >
+            Voltar para Home
+          </Button>
+        </Link>
+        <Button
+          variant="contained"
+          style={
+            breaker800px
+              ? {
+                  justifyContent: "center",
+                  height: 50,
+                  width: "100%",
+                  backgroundColor: theme.colors["main-green"],
+                }
+              : {
+                  backgroundColor: theme.colors["main-green"],
+                }
+          }
+          endIcon={<Check />}
+          size="large"
+          type="submit"
+        >
+          Finalizar
+        </Button>
+      </Box>
+    </form>
+  );
+};
